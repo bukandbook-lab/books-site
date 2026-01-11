c95bc13941361ebb69639e1015c383b9be7547aa
bukandbook-lab <bukandbook@gmail.com>
2026-01-11 18:10:22 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CART_ICON =
+"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjTsrBEOnV55lpyn2qOEcjDpsePZokQmZ-tQzYH5EHuXOOwNYJbrif6cohUTPCb6OMfol1HNUhHee19v70YOhqOBRUhzaKxZ_yycwoPReWbASOxS_y7m1vuJIHizFzHzgqUWf9C8LCnTq3NrW35-C_dYi8e_zZA-VrZ2vYRtaLgagu2aGsFIakxfWJLjo0_/s3500/pink%20cart.png";
+
+const DEFAULT_PRICE = 4;
