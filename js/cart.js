de9fdc34190a8623fa404ba429370d51ef8618e4
bukandbook-lab <bukandbook@gmail.com>
2026-01-15 13:43:44 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -390,3 +390,15 @@ document.addEventListener("click", e => {
     document.getElementById("orderForm")?.requestSubmit();
   }
 });
+/* =====================================
+   AUTO-GENERATE ORDER ID
+===================================== */
+function generateOrderId() {
+  const date = new Date();
+  const y = date.getFullYear();
+  const m = String(date.getMonth() + 1).padStart(2, "0");
+  const d = String(date.getDate()).padStart(2, "0");
+  const rand = Math.floor(1000 + Math.random() * 9000);
+
+  return `ORD-${y}${m}${d}-${rand}`;
+}
