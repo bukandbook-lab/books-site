601da8f2468e60236651e9db3a7eb31cdc0f557e
bukandbook-lab <bukandbook@gmail.com>
2026-01-17 22:05:22 +0800

    Update books.js

--- js/books.js
+++ js/books.js
@@ -91,6 +91,7 @@ function renderBooks(tabId, books) {
         data-book-id="${normalized.id}"
         data-title="${normalized.title}"
         data-price="${normalized.price}"
+        data-setqtty="${normalized.SetQtty}"
       >
     `;
 
