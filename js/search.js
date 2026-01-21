294d67ab19a5b9e0bb83581a8de504e3b815320f
bukandbook-lab <bukandbook@gmail.com>
2026-01-21 08:45:55 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -73,16 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
     showTab("BeginningReader");
   });
 
-    const searchInput = document.getElementById("bookSearch");
-    const clearBtn = document.getElementById("clearSearch");
 
-  if (searchInput && clearBtn) {
-    searchInput.addEventListener("input", () => {
-      clearBtn.style.display = searchInput.value ? "block" : "none";
-    });
-  }
-
- 
 
 
 });
