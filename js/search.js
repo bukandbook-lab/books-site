9e537476a459d9d6a609cd9770c4379bcfa09d67
bukandbook-lab <bukandbook@gmail.com>
2026-01-23 12:26:22 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -43,6 +43,11 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
+    // 🔍 GLOBAL SEARCH MODE → no active tab
+  document.querySelectorAll(".tab-btn")
+  .forEach(btn => btn.classList.remove("active"));
+
+
     hideTabs();
     const grid = getSearchGrid();
     grid.innerHTML = "";
