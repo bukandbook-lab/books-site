a8847aeb0e1ba549d41cb64fba541f27bb4a48de
bukandbook-lab <bukandbook@gmail.com>
2026-01-23 09:39:37 +0800

    Update data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -26,21 +26,24 @@ window.BOOKS_READY = Promise.all(
       .then(data => {
         ALL_BOOKS[category] = data;
 
-        data.forEach(book => {
-          BOOK_REGISTRY[book.id] = {
-            id: book.id || book.ID || book["Book ID"],
-            title: book.title || book["Book Title"] || "Untitled",
-            img: book.image || book.Link || "",
-            price: Number(book.price || book["Price"] || 0),
-            SetQtty: book.qtty || book["No. of Books"] || 0,
-            video:         
-               book["Youtube ID"] ||
-               book.youtube ||
-               book.video ||
-               null,
-        
-          };
-        });
+data.forEach(book => {
+  const id = book.id || book.ID || book["Book ID"];
+  if (!id) return;
+
+  BOOK_REGISTRY[id] = {
+    id,
+    title: book.title || book["Book Title"] || "Untitled",
+    img: book.image || book.Link || "",
+    price: Number(book.price || book["Price"] || 0),
+    SetQtty: book.qtty || book["No. of Books"] || 0,
+    video:
+      book["Youtube ID"] ||
+      book.youtube ||
+      book.video ||
+      null
+  };
+});
+
       })
   )
 ).then(() => {
