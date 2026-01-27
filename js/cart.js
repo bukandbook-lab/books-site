c7fa6c92a99ddfdd556f1e3d00b2d6e53194f342
bukandbook-lab <bukandbook@gmail.com>
2026-01-27 20:30:35 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -221,7 +221,8 @@ function renderCart() {
   
   box.innerHTML = `
    <span class="close-popup" alt="Close">X</span>
-   <h3>CART</h3>
+   <img width="200px" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi1aV6of-uDMKjQSWFV_lKbM_G5ogkqNXjjKfNk1V0VRRJJzQefQ0SgUSi3I7p-9ZtweZeQmZGu9cx6CFvdcvp5b7BxVdbGtQMsRq-DnCHY5hw1K2Z4gXkzMIyMkUUSDqQNWYevmQ_xi_u2NuxiTrLPstKBkTjjqPrsGL0VOiX0V9vfqxj_7Dc7b07WuLCt/s1600/header%202.jpg"/>
+   <span class="CartTitle">CART</span>
     ${itemsHTML || "<p>No item selected</p>"}
 
     
