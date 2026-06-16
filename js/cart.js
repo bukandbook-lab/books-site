9c521b1d29eace643986b490c85aa4c9dedd4aae
bukandbook-lab <bukandbook@gmail.com>
2026-06-16 12:57:55 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1262,7 +1262,7 @@ document.addEventListener("click", async e => {
     fileType: cart.fileType
   };
 
-  await fetch("https://script.google.com/macros/s/AKfycbzqYxWJoshG3TGmkdCRJXwgqmaJ5JOCQ3TSPjqzf4AAgbycRVpWWzPWk_4Yw0hoUWek/exec", {
+  await fetch("https://script.google.com/macros/s/AKfycbxyO7EdJE7oihVm-LTCRpkvpADbzOaBRU1HcP1uSfwiU0Mm-1iovZWJ8epxAdKviO07/exec", {
   method: "POST",
   body: JSON.stringify(payload),
   mode: "no-cors"
